fd39ec35171c6dc286359d83d8c53245b0ab3095
naftabaftab79-design <naftab.aftab79@gmail.com>
2026-05-13 02:40:51 +0500

    Fix exportCSV function syntax and formatting

--- js/exporter.js
+++ js/exporter.js
@@ -1,7 +1,6 @@
 function exportCSV(data) {
   const csv = Papa.unparse(data);
   const blob = new Blob([csv], { type: "text/csv" });
-
   const a = document.createElement("a");
   a.href = URL.createObjectURL(blob);
   a.download = "product_assets.csv";
